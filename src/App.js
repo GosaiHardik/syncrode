159e0f89e96e2bc63fe3d8e7922b74aafbfef97d
hardik.gosai <hardik.gosai@trinarybits.com>
2024-05-17 01:45:51 -0400

    [ADD] passcode

--- src/App.js
+++ src/App.js
@@ -7,21 +7,56 @@ import socketIOClient from 'socket.io-client';
 
 function App() {
 
+  const [passcode, setPasscode] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handlePasscodeChange = (event) => {
+    setPasscode(event.target.value);
+  };
+
+  const handlePasscodeSubmit = () => {
+    // Check the passcode here, for example:
+    if (passcode === '8642') {
+      setAuthenticated(true);
+    } else {
+      alert('Incorrect passcode. Please try again.');
+    }
+  };
+
   const [code, setCode] = useState('');
-  const socket = socketIOClient('http://localhost:3001');
+  const socket = socketIOClient('https://syncrode.brewingbinary.com');
 
 
   useEffect(() => {
+    socket.connect();
     socket.on('code change', (newCode) => {
       setCode(newCode);
     })
 
-    return () => socket.disconnect();
+    return () => {
+
+      setTimeout(() => {
+        console.log('Disconnecting from socket...');
+        socket.disconnect();
+      }, 1000);
+
+    }
   })
 
   const handleCodeChange = (newCode) => {
     setCode(newCode);
-    socket.emit('code change', newCode);  
+    socket.emit('code change', newCode);
+  }
+
+  if (!authenticated) {
+    return (
+      <div className='login'>
+        <div>
+          <input type="password" placeholder='Enter Passcode' value={passcode} onChange={handlePasscodeChange} />
+          <button onClick={handlePasscodeSubmit}>Submit</button>
+        </div>
+      </div>
+    );
   }
 
   return (
--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -3,14 +3,16 @@
 // import React, { useState } from 'react';
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { html } from '@codemirror/lang-html';
 
 const CodeEditor = ({ code, onChange }) => {
   return (
     <CodeMirror
       value={code}
-      height="800px"
+      height="88vh"
       theme={vscodeDark}
       onChange={onChange}
+      extensions={[html({ jsx: true })]}
     />
   );
 };
